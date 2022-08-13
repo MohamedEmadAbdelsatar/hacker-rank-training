34bab38a0ec41f862b31ff431736adaabeb00863
Mohamed Emad <medo_emad2011@yahoo.com>
2022-08-13 14:21:43 +0200

    Create linked list implementation.js

---
+++ linked list implementation.js
@@ -0,0 +1,24 @@
+'use strict';
+
+class ListNode{
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+    constructor(head = null) {
+        this.head = head;
+    }
+}
+
+function main() {
+  let node1 = new ListNode(2);
+  let node2 = new ListNode(5);
+  node1.next = node2;
+  
+  let list = new LinkedList(node1);
+  
+  console.log(list.head.next.data);
+}
